057426d2adfe22d7cd0f2ddea6e2e11e7c458846
excela01 <excela01@gmail.com>
2021-08-19 23:55:20 +1245

    firt commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//$(document).ready(function() {
+//    const printName = (name) =>{
+//        document.getElementById("name").innerHTML=name
+//    }
+//    printName("Excel Amaefule Obumneme");
+//})
+//
+// const printName = (name) =>{
+//        document.getElementById("name").innerHTML=name
+//    }
+//    printName("Excel Amaefule Obumneme");
+
+
+alert("Excel Amaefule Obumneme")
